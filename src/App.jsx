a41b34e04d337fc9938d95d3e078b4b979804fe0
Victor <aue228lublupacanov@gmail.com>
2023-10-21 20:39:52 +0300

    step2 is done

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
   const handleFeedbackGood = () => {
     setGood(good + 1)
+    setTotal(total + 1)
 
   }
   const handleFeedbackNeutral = () => {
     setNeutral(neutral + 1)
+    setTotal(total + 1)
 
   }
   const handleFeedbackBad = () => {
     setBad(bad + 1)
+    setTotal(total + 1)
 
   }
 
@@ -30,6 +35,9 @@ const App = () => {
     <p>good {good}</p>
     <p>neutral {neutral}</p>
     <p>bad {bad}</p>
+    <p>all {total}</p>
+    <p>average {(good - bad) / total}</p>
+    <p>positive {(good / total) * 100}%</p>
   </div>)
 }
 
