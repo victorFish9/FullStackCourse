b8368ae2086c7c4c8e13fbcd9c04c648373865ed
Victor <aue228lublupacanov@gmail.com>
2023-12-04 16:54:28 +0200

    upload rest of file

---
+++ practice/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Note from './components/Note'
+
+const App = ({ notes }) => {
+
+  const result = notes.map(note => note.id)
+  console.log("Result: ", result)
+
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note =>
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+    </div>
+  )
+}
+export default App
