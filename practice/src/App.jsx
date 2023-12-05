57892d4e476543418efb184d658febb2f0d842ee
Victor <aue228lublupacanov@gmail.com>
2023-12-05 16:40:23 +0200

    practiced with axios and useEffect

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,13 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 import Note from './components/Note'
 
 const App = (props) => {
 
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('a new note...')
-  const result = notes.map(note => note.id)
-  console.log("Result: ", result)
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    console.log('effect')
+    axios.get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+  useEffect(hook, [])
+
+  console.log('render', notes.length, 'notes')
+
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
 
   const addNote = (event) => {
     event.preventDefault()
@@ -31,8 +48,13 @@ const App = (props) => {
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map(note =>
+        {notesToShow.map(note =>
           <Note key={note.id} note={note} />
         )}
       </ul>
