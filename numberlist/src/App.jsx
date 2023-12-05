ac65d1fadb9365556218ba77396f4de403e0970f
Victor <aue228lublupacanov@gmail.com>
2023-12-05 20:41:08 +0200

    changed object list to db json

--- numberlist/src/App.jsx
+++ numberlist/src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import AddPerson from './components/AddPerson'
 import FilterPerson from './components/FilterPerson'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '04440000' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterName] = useState('')
+
+  const hook = () => {
+    console.log("effect")
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        console.log("promise fulfilled", response)
+        setPersons(response.data)
+      })
+  }
+
+  useEffect(hook, [])
 
 
   const handleFilterName = (event) => {
